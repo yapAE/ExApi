18ef2f50bf70bbf4db9d6c2bbd691f7577f22941
Terry <lihao@bidangjia.io>
2019-08-26 14:38:10 +0800

    models

--- api/__init__.py
+++ api/__init__.py
@@ -1,25 +1,4 @@
-from flask import jsonify
-
-
-def validsign(func):
-    “”“
-    验证签名
-    ”“”
-
-    def decorator():
-        params = _get_request_params()
-        appkey = params.get('appkey')
-        sign = params.get('sign')
-        csign = signature(params)
-        if not appkey:
-            return make_response_error(300, 'appkey is none.')
-        if csign != sign:
-            return make_response_error(500, 'signature is error.')
-
-        return func()
-
-
-return decorator
+from flask import jsonify, request
 
 
 def _get_request_param(key):
