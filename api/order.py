88ef78d4bbf279f2f9a12cb66b7a2899a366045a
Terry <lihao@bidangjia.io>
2019-08-27 16:24:11 +0800

    trades

--- api/order.py
+++ api/order.py
@@ -5,9 +5,18 @@ from api import make_response_ok, create_exchange
 _order = Blueprint("order", __name__, url_prefix='/api/v1')
 
 
-@_order.route("order_book/<path:symbol>", methods=['GET'], endpoint="order")
+@_order.route("order-book/<path:symbol>", methods=['GET'], endpoint="order")
 def order(symbol):
-    exchange_name = request.args.get('ex_id')
+    exchange_name = request.args.get('ex')
     exchange = create_exchange(exchange_name)
     order_book = exchange.fetch_order_book(symbol)
     return make_response_ok(order_book)
+
+
+@_order.route("trades/<path:symbol>", methods=['GET'], endpoint='trades')
+def trades(symbol):
+    exchange_name = request.args.get('ex')
+    limit = request.args.get('limit')
+    exchang = create_exchange(exchange_name)
+    trades = exchang.fetch_trades(symbol, limit)
+    return make_response_ok(trades)
